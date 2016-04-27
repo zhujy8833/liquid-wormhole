0702085f07f12e9a78c9f31c40b765a7f6c0a72b
David Pett <dpett@folklore.digital>
2016-04-27 13:43:44 -0500

    filtered initializers

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 /* jshint node: true */
 'use strict';
 
+var filterInitializers = require('fastboot-filter-initializers');
+
 module.exports = {
-  name: 'liquid-wormhole'
+  name: 'liquid-wormhole',
+
+  preconcatTree: function(tree) {
+    return filterInitializers(tree, this.app.name);
+  }
 };
